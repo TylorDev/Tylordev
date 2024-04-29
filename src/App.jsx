4b2e82ca130b5b4768fedd8598f4f36939d31bd2
TylorDev <jhontmg26@gmail.com>
2024-04-29 14:22:08 -0300

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 /* eslint-disable react/prop-types */
 import "./App.scss";
 
-import { MousePositionProvider } from "./Context/MouseContext";
-import Gallery from "./Components/Gallery";
-import { DragProvider } from "./Context/DragContext";
-
 function App() {
   return (
     <>
-      <MousePositionProvider>
-        <DragProvider>
-          <Gallery></Gallery>
-        </DragProvider>
-      </MousePositionProvider>
+      <div></div>
     </>
   );
 }
